3d833d816e8c8c8ef920f850e36deb998f05dc15
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2024-08-10 12:20:53 -0400

    deployment

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,8 @@
 
 import sys
-# current_path = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(os.path.join(current_path, "lib"))
-#
-# from app import app as application
-
-# path = '/home/antinomy/testenv'
-# if path not in sys.path:
-#     sys.path.append(path)
-#
-
-# from quick_session import app as application
-
-
 
 project_home = '/home/sillybilly777/UX-Reviews-AppStore/src'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-from my_flask_app import app as application
-
-# from src import app
-#
-# if __name__ == "__main__":
-#     app.run()
-
-
-# project_home = u'/home/DrAgus/mysite'
-# path = 'my_flask_app/src/app'
-# if path not in sys.path:
-#    sys.path.insert(0, path)
-#
-# from app import app as application
+from app import app as application
