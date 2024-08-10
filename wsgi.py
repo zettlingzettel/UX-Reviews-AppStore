bd861d482482149298ce9aefb60f54322af86f3b
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2024-08-10 12:25:20 -0400

    deployment

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 import sys
 
 project_home = '/home/sillybilly777/UX-Reviews-AppStore/src'
-if project_home not in sys.path:
+if path not in sys.path:
     sys.path.insert(0, project_home)
 
 from app import app as application
