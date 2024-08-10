28432a814693cd208ad0a11e682a3c1e710f6ccb
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2024-08-10 13:02:56 -0400

    deployment

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,43 @@
 
+# import sys
+
+# project_home = '/home/sillybilly777/UX-Reviews-AppStore'
+# if project_home not in sys.path:
+#     sys.path.insert(0, project_home)
+
+# from app import app as application
+
 import sys
+#
+## The "/home/alinecatikoc" below specifies your home
+## directory -- the rest should be the directory you uploaded your Flask
+## code to underneath the home directory.  So if you just ran
+## "git clone git@github.com/myusername/myproject.git"
+## ...or uploaded files to the directory "myproject", then you should
+## specify "/home/alinecatikoc/myproject"
 
-project_home = '/home/sillybilly777/UX-Reviews-AppStore/src'
+
+
+path = "/home/sillybilly777/UX-Reviews-AppStore"
 if path not in sys.path:
-    sys.path.insert(0, project_home)
+    sys.path.append(path)
 
-from app import app as application
+from .src import app as application
+# path = '/home/sillybilly777/UX-Reviews-AppStore'
+# if path not in sys.path:
+#   sys.path.insert(path)
+
+# from app import app as application
+# try:
+#     from app import app as application
+#     print("Import successful!")
+# except ImportError as e:
+#     print(f"Import failed: {e}")
+
+# import os
+# from dotenv import load_dotenv
+# project_folder = os.path.expanduser(path)  # adjust as appropriate
+# load_dotenv(os.path.join(project_folder, '.env'))
+
+# from app import app as application
+# from flaskai import app as application
